4f868e955e49bdce96e2a45ba09162eb200c9fe7
valeriapvv <poklonovavaleria@gmail.com>
2023-06-27 21:08:53 +0700

    Рендерит компоненты фильтров и сортировки

--- src/presenter/main-screen-presenter.js
+++ src/presenter/main-screen-presenter.js
@@ -1,5 +1,7 @@
 import FilmsSectionView from '../view/films-section-view.js';
 import FilmsPresenter from './films-presenter.js';
+import FiltersView from '../view/filters-view.js';
+import SortView from '../view/sort-view.js';
 import {render} from '../render.js';
 
 export default class MainScreenPresenter {
@@ -8,6 +10,20 @@ export default class MainScreenPresenter {
   }
 
   init() {
+    this.renderFilters();
+    this.renderSort();
+    this.renderFilmsSection();
+  }
+
+  renderFilters() {
+    render(new FiltersView(), this.parentElement);
+  }
+
+  renderSort() {
+    render(new SortView(), this.parentElement);
+  }
+
+  renderFilmsSection() {
     this.filmsSectionComponent = new FilmsSectionView();
 
     this.filmsPresenter = new FilmsPresenter({
