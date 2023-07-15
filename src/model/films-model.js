d32cc7997bddfb580e633486c66f17a66f2b9b32
valeriapvv <poklonovavaleria@gmail.com>
2023-07-15 20:33:37 +0700

    Refactor: Model. WIP

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -2,13 +2,13 @@ import {FILMS_COUNT} from '../mock/constants.js';
 import {generateFilm} from '../mock/film.js';
 
 export default class FilmsModel {
-  films = null;
+  #films = null;
 
-  getFilms() {
-    if (this.films === null) {
-      this.films = Array.from({length: FILMS_COUNT}, generateFilm);
+  get films() {
+    if (this.#films === null) {
+      this.#films = Array.from({length: FILMS_COUNT}, generateFilm);
     }
 
-    return this.films;
+    return [...this.#films];
   }
 }
